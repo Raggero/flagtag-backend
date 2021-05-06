d4243f257854e94ee82bdeef10e9242a1489f6fc
Helena <helenalundstrom@hotmail.se>
2021-05-06 13:03:22 +0200

    Added post method and validation. Installed npm express-validator

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const db = require("./database.js");
 const bodyParser = require("body-parser");
+const { body, validationResult } = require('express-validator');
 
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -34,6 +35,7 @@ app.get("/users", (req, res, next) => {
 app.get("/users/:name/:password", (req, res, next) => {
     var searchQuery = "select * from users where userName = ? AND userPassword = ?"
     var params =[req.params.name, req.params.password]
+    console.log(params);
     db.all(searchQuery, params, (error, rows) => {
         if (error) {
             res.status(400).json({"error":error.message});
@@ -44,5 +46,39 @@ app.get("/users/:name/:password", (req, res, next) => {
         })
     });
 });
+
+app.post("/users",
+    body('username').isLength({min: 2}),
+    body('password').isLength({min: 2}),
+    (req, res, next) => {
+        console.log(req.body.username);
+        console.log(req.body.password);
+
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({
+                success: false,
+                errors: errors.array()
+            })
+        }
+
+        var insertQuery ='INSERT INTO users (userName, userPassword) VALUES (?,?)'
+        var params =[req.body.username, req.body.password]
+        db.run(insertQuery, params, function (err, result) {
+            if (err){
+                res.status(400).json({"error": err.message})
+                return;
+            }
+            res.json({
+                success: true,
+                message: 'Login successful',
+                "users": {
+                    "userId" : this.lastID,
+                    username: req.body.username,
+                    password: req.body.password
+                }
+            })
+        });
+    });
 
 
